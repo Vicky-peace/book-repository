20e40c5b3855285effe8d2d8dfbc869271485187
Vicky-peace <victorpeace47@gmail.com>
2024-06-26 19:22:22 +0300

    refactored search

--- src/components/bookitem/BookItem.tsx
+++ src/components/bookitem/BookItem.tsx
@@ -13,8 +13,8 @@ function BookItem({ book, onEdit, onDelete }: BookItemProps) {
       <td>{book.author}</td>
       <td>{book.year}</td>
       <td className='actions'>
-        <button onClick={() => onEdit(book)}>Edit</button>
-        <button onClick={() => onDelete(book.id)}>Delete</button>
+        <button onClick={() => onEdit(book)} className='edit'>Edit</button>
+        <button onClick={() => onDelete(book.id)} className='delete'>Delete</button>
       </td>
     </tr>
   );
